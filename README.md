4aba75da6da7c27f34fa492994bda259e33f059a
Uli <root@localhost>
2017-04-23 17:10:56 +0800

    changed

---
+++ README.md
@@ -0,0 +1,1 @@
+Sample Dockerfile and minimal app for displaying Hello World using Python and Flask
---
+++ build_container.sh
@@ -0,0 +1,4 @@
+# Build the container from the files in the current directory
+# replace with your handle/tag
+
+docker build -t u1i/helloworld .
